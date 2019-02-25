204d2e96f4ffc2c6be7c7e3b398a43b9a3e587b5
akseyh <akgulmuhammed@live.com>
2019-02-25 17:10:57 +0300

    veriler adinda bir dictionary olusturup kisiyi, en cok kullandigi kelimeyi ve kelimeyi kullanma sikligini bu dictionary'e atiyor.

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-import re,json
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
 
 p = re.compile("(\d{2}.\d{2}.\d{4})\s(\d{2}:\d{2})\s-\s([A-Za-zıİöÖüÜçÇşŞğĞ\s]*):\s(.*)")
 
 textFile = "deneme.txt"
+data = []
+veriler = {}
 
 text = open(textFile,"r")
-metin = str
-data = []
-lineCount = 0
-
-# line counter
 while True:
-    txtLine = text.readline()
-    if( txtLine ):
-        lineCount += 1
+    metin = text.readline()
+    if( metin ):
+        data.append(
+            (p.search(metin).group(1),
+            p.search(metin).group(2),
+            p.search(metin).group(3),
+            p.search(metin).group(4))
+        )
     else:
         break
-text.close()
-
-text = open(textFile,"r")
-for i in range(lineCount):
-    metin = text.readline()
-    if( p.search(metin) is None):
-        continue
-    data.append(
-        (p.search(metin).group(1),
-        p.search(metin).group(2),
-        p.search(metin).group(3),
-        p.search(metin).group(4))
-    )
 
 kelimeler = {}
 for r in data:
@@ -42,9 +33,10 @@ for r in data:
     name = name.replace('ö', 'o')
     name = name.replace('ü', 'u')
     kelimeler[name] = {}
+    veriler[name] = {}
 
-for i in data:
-    name = str(i[2].replace(" ","-"))
+for satir in data:
+    name = str(satir[2].replace(" ","-"))
     name = name.replace('ş', 's')
     name = name.replace('ğ', 'g')
     name = name.replace('ç', 'c')
@@ -53,7 +45,7 @@ for i in data:
     name = name.replace('ö', 'o')
     name = name.replace('ü', 'u')
 
-    for j in i[3].split():
+    for j in satir[3].split():
         j = j.replace("İ","i")
         j = j.lower()
         if j not in kelimeler[name]:
@@ -64,15 +56,14 @@ for i in data:
             kelimeler[name].update({ j: int(kelimeler[name].get(j)+1) }) 
 
 for kullanici in kelimeler:
-    kontrol = 0
+    sayac = 0
     word = ""
     for kelime in kelimeler[kullanici]:
-        if(kelimeler[kullanici].get(kelime)>kontrol):
-            kontrol = kelimeler[kullanici].get(kelime)
+        if(kelimeler[kullanici].get(kelime)>sayac):
+            sayac = kelimeler[kullanici].get(kelime)
             word = kelime
-    print(kullanici)
-    print(word)
-    print(kontrol)
+    veriler[kullanici].update({word:sayac})
 
+print(veriler)
 
 text.close()
