cef80e7ea4221b4e871d92aa973a111ec2967e41
akseyh <akgulmuhammed@live.com>
2019-03-10 01:02:01 +0300

    code edited and translated variables name

--- app.py
+++ app.py
@@ -1,63 +1,45 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import re
-
 p = re.compile("(\d{2}.\d{2}.\d{4})\s(\d{2}:\d{2})\s-\s([A-Za-zıİöÖüÜçÇşŞğĞ\s]*):\s(.*)")
 
 textFile = "deneme.txt"
-data = []
-veriler = {}
+result, words, data = {}, {}, []
+turkishCharacters = { 'ş':'s', 'ğ':'g', 'ç':'c', 'İ':'i', 'ı':'i', 'ö':'o', 'ü':'u', ' ':'-' }
 
 text = open(textFile,"r")
 
 while True:
-    metin = text.readline()
-    if(not metin):
-        break
-    if( re.search(p,metin) ):
-        data.append(
-                (p.search(metin).group(1),
-                p.search(metin).group(2),
-                p.search(metin).group(3),
-                p.search(metin).group(4))
-        )
-    else:
-        continue
-        
+    line = str( text.readline() )
+    if( not line ): break
+    if( not re.search(p,line) ): continue
 
-kelimeler = {}
+    data.append( (p.search(line).group(1), p.search(line).group(2), p.search(line).group(3), p.search(line).group(4)) )    
 
-for satir in data:
-    name = str(satir[2].replace(" ","-"))
-    name = name.replace('ş', 's')
-    name = name.replace('ğ', 'g')
-    name = name.replace('ç', 'c')
-    name = name.replace('İ', 'i')
-    name = name.replace('ı', 'i')
-    name = name.replace('ö', 'o')
-    name = name.replace('ü', 'u')
+for element in data:
+    name = str(element[2])
+    for key in turkishCharacters:
+        name = name.replace(key, turkishCharacters.get(key))
 
-    if( name not in kelimeler ):
-        kelimeler[name] = {}
-        veriler[name] = {}
+    if( name not in words ):
+        words[name] = {}
+        result[name] = {}
 
-    for kelime in satir[3].split():
-        kelime = kelime.replace("İ","i")
-        kelime = kelime.lower()
-        if kelime not in kelimeler[name]:
-            kelimeler[name].update({
-                kelime:1
+    for word in element[3].split():
+        word = word.lower()
+        if word not in words[name]:
+            words[name].update({
+                word:1
             })
-        else:
-            kelimeler[name].update({ kelime: int(kelimeler[name].get(kelime)+1) }) 
+        else: words[name].update({ word: int(words[name].get(word)+1) }) 
 
 # En çok geçen kelimenin bulunması
-# kelimeler veriler dictionary'sine atılıyor. istenen kullanicinin kelimesi veriler[kullanici] ile cekilebilir.
-for kullanici in kelimeler.keys():
-    sorted_words = sorted(kelimeler[kullanici],key=kelimeler[kullanici].get)
-    word_frequency = kelimeler[kullanici].get(sorted_words[-1])
-    veriler[kullanici].update({ sorted_words[-1] : word_frequency })
+# kelimeler result dictionary'sine atılıyor. istenen kullanicinin kelimesi result[users] ile cekilebilir.
+for user in words.keys():
+    sorted_words = sorted(words[user],key=words[user].get)
+    word_frequency = words[user].get(sorted_words[-1])
+    result[user].update({ sorted_words[-1] : word_frequency })
 
-print(veriler)
+print(result)
 
 text.close()
